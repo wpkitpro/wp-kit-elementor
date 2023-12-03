386ce7b03fb564041e4a88d1bb9725213a28baca
Garik <Garik.28H@gmail.com>
2023-12-03 16:18:14 +0400

    Add footer documentation to footer.php
    Added block comments at the top of the footer.php file to describe the purpose of the file, comply with Wordpress standards, and ensure clarity for developers. The URL assigned to $theme_url had its trailing slash removed for consistency.

--- footer.php
+++ footer.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WP Kit Elementor
+ * @since WP Kit Elementor 1.0
+ */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
@@ -6,7 +16,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 $theme_name   = wp_get_theme()->get( 'Name' );
 $theme_author = wp_get_theme()->get( 'Author' );
-$theme_url    = 'https://wpkit.pro/';
+$theme_url    = 'https://wpkit.pro';
 ?>
 </main><!-- #main -->
 
