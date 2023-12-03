20e82dc9d2609529098f0047260ccc39396be33b
Garik <Garik.28H@gmail.com>
2023-12-03 20:31:15 +0400

    Update template structure and improve accessibility in page.php
    The structure of the 'page.php' template of WP Kit Elementor theme has been updated to improve semantics and accessibility. The changes include:
    - Addition of a check to exit if the file is accessed directly for improved security.
    - Use of 'div' instead of 'main' as main content wrapper, improving HTML semantics for better representation.
    - A shift in the sequence of template parts being called for a more logical layout rendering.
    These modifications aim to enhance the overall user experience and adherence to best practices in web development.

--- page.php
+++ page.php
@@ -1,14 +1,25 @@
 <?php
 /**
  * The template for displaying all single posts
+ *
+ * @package WPKitElementor
  */
 
-get_header();
-
-while ( have_posts() ) {
-	the_post();
-	get_template_part( 'template-parts/header/page-header' );
-	get_template_part( 'template-parts/content/content-page' );
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
 }
 
-get_footer();
+get_header();
+?>
+<div id="primary" class="content-area">
+	<main id="main" class="site-main">
+		<?php
+		while ( have_posts() ) {
+			the_post();
+			get_template_part( 'template-parts/header/page-header' );
+			get_template_part( 'template-parts/content/content-page' );
+		}
+		?>
+	</main><!-- #main -->
+</div><!-- #primary -->
+<?php get_footer(); ?>
