edf415e4048b6ad98a023d61eb408bcd445fa1de
garikhg <garik.28h@gmail.com>
2023-12-11 20:56:23 +0400

    Update text domain in 404.php
    Updated the text domain from 'wpkit' to 'wpkit-elementor' in the 404.php file of the WPKit Elementor theme. This ensures consistency across all theme files and enables better internationalization support.

--- 404.php
+++ 404.php
@@ -2,12 +2,12 @@
 
 <div class="container">
   <header class="page-header alignwide">
-    <h1 class="page-title"><?php esc_html_e( 'Nothing here', 'wpkit' ); ?></h1>
+    <h1 class="page-title"><?php esc_html_e( 'Nothing here', 'wpkit-elementor' ); ?></h1>
   </header>
 
   <div class="error-404 not-found default-max-width">
     <div class="page-content">
-      <p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try a search?', 'wpkit' ); ?></p>
+      <p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try a search?', 'wpkit-elementor' ); ?></p>
       <?php get_search_form(); ?>
     </div>
   </div>
