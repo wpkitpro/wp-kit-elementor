ffa21635daa29ccf5dac5c6c1380030feded6c1e
garikhg <garik.28h@gmail.com>
2023-12-21 23:53:14 +0400

    Revert theme version to 1.0.0 and refactor header implementation
    In this commit, the version of the theme in both style.css and style.scss files has been changed back to 1.0.0 from 1.0.2. The code related to the site's header, which was previously located in a separate file (header.php), is now directly included in the main header file for simpler file structure. Also, the readme text has been simplified and the package.json file has been updated for improved exclusion handling in bundle script.

--- header.php
+++ header.php
@@ -10,6 +10,9 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
+
+$blog_name        = get_bloginfo( 'name' );
+$blog_description = get_bloginfo( 'description', 'display' );
 ?><!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
@@ -26,7 +29,36 @@ if ( ! defined( 'ABSPATH' ) ) {
 		<?php esc_html_e( 'Skip to content', 'wp-kit-elementor' ); ?>
 	</a>
 
-	<?php get_template_part( 'template-parts/header' ); ?>
+	<header id="masthead" class="site-header" role="banner">
+		<div class="site-logo">
+			<?php if ( has_custom_logo() ): ?>
+				<?php the_custom_logo(); ?>
+			<?php elseif ( $blog_name ): ?>
+				<h1 class="site-title">
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>"
+					   title="<?php echo esc_attr__( 'Home', 'wp-kit-elementor' ); ?>"
+					   rel="home"
+					>
+						<?php echo $blog_name; ?>
+					</a>
+				</h1>
+				<?php if ( $blog_description ): ?>
+					<p class="site-description">
+						<?php echo esc_html( $blog_description ); ?>
+					</p>
+				<?php endif; // Endif $blog_description ?>
+
+			<?php endif; // Endif has_custom_logo() ?>
+		</div>
+		<nav id="site-navigation"
+			 class="primary-navigation"
+			 aria-label="<?php esc_attr_e( 'Primary menu', 'wp-kit-elementor' ); ?>"
+		>
+			<?php if ( has_nav_menu( 'primary' ) ) { ?>
+				<?php wp_nav_menu( [ 'theme_location' => 'primary' ] ); ?>
+			<?php } // Endif has_nav_menu('primary') ?>
+		</nav>
+	</header>
 
 	<div id="content" class="site-content">
 
