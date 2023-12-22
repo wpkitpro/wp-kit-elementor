ff5e6df97966f5f2d93422a9a891b8df3fbc387e
garikhg <garik.28h@gmail.com>
2023-12-22 23:13:15 +0400

    Added editor style support to wp-kit-elementor theme
    Added support for 'search-form', 'script', and 'style' to the html5 theme support in the functions.php file of the wp-kit-elementor theme. Also added the 'style-editor.css' file and the 'style-editor.scss' file. Integrated the processing of 'style-editor.scss' into 'webpack.mix.js'. The 'style-editor.css' and 'style-editor.scss' files contain instructions not to modify them directly and to use a custom directory for changes.

--- functions.php
+++ functions.php
@@ -39,11 +39,19 @@ function setup() {
 
 	// Switch default core markup for search form, comment form, and comments to output valid HTML5
 	add_theme_support( 'html5', array(
+		'search-form',
 		'comment-form',
 		'comment-list',
 		'gallery',
 		'caption',
+		'script',
+		'style',
 	) );
+
+	/*
+	 * Editor Style.
+	 */
+	add_editor_style( 'style-editor.css' );
 
 	// Gutenberg Embeds
 	add_theme_support( 'responsive-embeds' );
