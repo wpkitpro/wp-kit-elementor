315d8b5476b0910997ff5e168e90830da76b48b7
garikhg <garik.28h@gmail.com>
2023-12-22 22:53:07 +0400

    Create header template in wp-kit-elementor theme
    Modularized the wp-kit-elementor theme by introducing a separate template part for headers. This enhances code reusability and cleans up the main header.php file, maintaining only the necessary HTML structure in header.php and moving the site logo and navigation details to the new template-part/header.php.

---
+++ template-parts/header.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * The template for displaying header.
+ *
+ * @package WpKitElementor
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+$blog_name        = get_bloginfo( 'name' );
+$blog_description = get_bloginfo( 'description', 'display' );
+?>
+<header id="masthead" class="site-header" role="banner">
+	<div class="site-logo">
+		<?php if ( has_custom_logo() ): ?>
+			<?php the_custom_logo(); ?>
+		<?php elseif ( $blog_name ): ?>
+			<h1 class="site-title">
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>"
+				   title="<?php echo esc_attr__( 'Home',
+					   'wp-kit-elementor' ); ?>"
+				   rel="home"
+				>
+					<?php echo $blog_name; ?>
+				</a>
+			</h1>
+			<?php if ( $blog_description ): ?>
+				<p class="site-description">
+					<?php echo esc_html( $blog_description ); ?>
+				</p>
+			<?php endif; // Endif $blog_description ?>
+
+		<?php endif; // Endif has_custom_logo() ?>
+	</div>
+	<nav id="site-navigation"
+		 class="primary-navigation"
+		 aria-label="<?php esc_attr_e( 'Primary menu', 'wp-kit-elementor' ); ?>"
+	>
+		<?php if ( has_nav_menu( 'primary' ) ) { ?>
+			<?php wp_nav_menu( [ 'theme_location' => 'primary' ] ); ?>
+		<?php } // Endif has_nav_menu('primary') ?>
+	</nav>
+</header>
