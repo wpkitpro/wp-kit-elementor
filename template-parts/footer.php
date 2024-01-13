ffbb9700f30613166aa313c1ce5b652ce6bb6ee7
garikhg <garik.28h@gmail.com>
2024-01-13 22:49:42 +0400

    Refactor footer theme design in 'wp-kit-elementor'
    Footer code has been moved to a separate 'template-parts/footer' file to improve code organization in the 'wp-kit-elementor' theme. The footer now consists of footer navigation and credit section, enhancing the structural clarity of the theme's codebase.

---
+++ template-parts/footer.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * The template for displaying footer.
+ *
+ * @package WpKitElementor
+ */
+
+$theme_name   = wp_get_theme()->get( 'Name' );
+$theme_author = wp_get_theme()->get( 'Author' );
+$theme_url    = wp_get_theme()->get( 'AuthorURI' );
+
+$footer_nav_menu = wp_nav_menu( array(
+	'theme_location' => 'footer',
+	'fallback_cb'    => false,
+	'echo'           => false,
+) );
+?>
+<footer id="colophon" class="site-footer">
+
+	<?php if ( $footer_nav_menu ): ?>
+		<nav class="site-navigation">
+			<?php
+			// PHPCS - escaped by WordPress with "wp_nav_menu"
+			echo $footer_nav_menu;  // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+			?>
+		</nav>
+	<?php endif; ?>
+
+	<div class="footer-copyright">
+
+		<div class="credit">
+			<?php
+			/* translators: %1$s: theme name, %2$s link, %3$s theme author */
+			echo sprintf( __( '%1$s Theme by <a href="%2$s">%3$s.</a>',
+				'wp-kit-elementor' ), esc_html( $theme_name ),
+				esc_url( $theme_url ),
+				$theme_author
+			);
+			?>
+		</div>
+
+	</div>
+
+</footer><!-- #colophon -->
