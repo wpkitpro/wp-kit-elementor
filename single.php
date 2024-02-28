2a99389224be6707c8618afdf5ac7920bf813623
garikhg <garik.28h@gmail.com>
2024-02-29 00:17:34 +0400

    Update single post template structure
    Reworked the structure of the single post template in the wp-kit-elementor theme. Changes include wrapping everything in a 'site-main' tag instead of 'post' ID, changing 'entry-content' to 'page-content', and adding a 'post-tags' div. This should enhance webpage layout and management.

--- single.php
+++ single.php
@@ -5,14 +5,21 @@
 
 get_header();
 ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-  <header class="entry-header">
-    <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-  </header>
+<main id="content" <?php post_class( 'site-main' ); ?>>
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	</header>
 
-  <div class="entry-content">
-    <?php the_content(); ?>
-  </div>
-</article>
+	<div class="page-content">
+		<?php the_content(); ?>
+
+		<div class="post-tags">
+			<?php the_tags( '<span class="tag-links">' . esc_html__( 'Tagged', 'wp-kit-elementor' ), null, '</span>' ); ?>
+		</div>
+		<?php wp_link_pages() ?>
+	</div>
+
+	<?php comments_template(); ?>
+</main>
 
 <?php get_footer(); ?>
